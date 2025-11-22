72db2b868c57fc7475c1d5971c6ba1dcdba371be
Cayden Wright <cwright2022@gmail.com>
2025-11-22 12:48:19 -0500

    skip download if dataset exists

--- load_data.py
+++ load_data.py
@@ -1,12 +1,18 @@
 # loads dataset from kagglehub
 import kagglehub
 import pandas as pd
+import shutil
+import os
 
 def load_dataset() -> pd.DataFrame:
-    # Download latest version
-    path = kagglehub.dataset_download("advaitnmenon/network-traffic-data-malicious-activity-detection")
-    print("Downloaded dataset to:", path)
-    data_frame = pd.read_csv(path+"/NandakumarMenonAdvait_MT_S2.csv")
+    if os.path.exists("./dataset/NandakumarMenonAdvait_MT_S2.csv"):
+        print("Datset exists in folder. Skipping download.")
+    else:
+        # Download latest version
+        path = kagglehub.dataset_download("advaitnmenon/network-traffic-data-malicious-activity-detection")
+        print("Downloaded dataset to:", path)
+        shutil.move(path+"/NandakumarMenonAdvait_MT_S2.csv", "./dataset/NandakumarMenonAdvait_MT_S2.csv")
+    data_frame = pd.read_csv("./dataset/NandakumarMenonAdvait_MT_S2.csv")
     return data_frame
 
 def describe_dataset(df: pd.DataFrame) -> None:
