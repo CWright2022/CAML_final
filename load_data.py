ae52c64890c7bbb80ae21ac6e02d9ad5bc8856ba
zriback <zacharythecdr@gmail.com>
2025-11-23 22:02:21 -0500

    Switch to using malicious URLs dataset

--- load_data.py
+++ load_data.py
@@ -4,23 +4,19 @@ import pandas as pd
 import shutil
 import os
 
+DATASET_PATH = "./dataset/malicious_phish.csv"
+
 def load_dataset() -> pd.DataFrame:
-    if os.path.exists("./dataset/NandakumarMenonAdvait_MT_S2.csv"):
+    if os.path.exists(DATASET_PATH):
         print("Datset exists in folder. Skipping download.")
     else:
         # Download latest version
-        path = kagglehub.dataset_download("advaitnmenon/network-traffic-data-malicious-activity-detection")
+        path = kagglehub.dataset_download("sid321axn/malicious-urls-dataset")
         print("Downloaded dataset to:", path)
-        shutil.move(path+"/NandakumarMenonAdvait_MT_S2.csv", "./dataset/NandakumarMenonAdvait_MT_S2.csv")
-    data_frame = pd.read_csv("./dataset/NandakumarMenonAdvait_MT_S2.csv")
+        shutil.move(path+"/malicious_phish.csv", DATASET_PATH)
+    data_frame = pd.read_csv(DATASET_PATH)
     return data_frame
 
 def describe_dataset(df: pd.DataFrame) -> None:
     print("Dataset Description:")
     print(df.describe())
-    print("rows x columns:")
-    print(df.shape)
-    print("preview:")
-    print(df.head())
-    print("class overview:")
-    print(df["bad_packet"].value_counts())
