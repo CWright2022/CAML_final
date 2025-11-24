afe03aaf96496ee4475aca8b1dc4bf58deb089fb
zriback <zacharythecdr@gmail.com>
2025-11-24 01:25:06 -0500

    Add function to clean the dataframe

--- load_data.py
+++ load_data.py
@@ -6,6 +6,13 @@ import os
 
 DATASET_PATH = "./dataset/malicious_phish.csv"
 
+def clean_dataset(df: pd.DataFrame) -> pd.DataFrame:
+    """Ensure URLs are formed correctly and remove duplicates"""
+    df_clean = df.copy()
+    df_clean = df_clean[df_clean['url'].str.match(r'^[\x00-\x7F]+$', na=False)]
+    df_clean = df_clean.drop_duplicates(subset=['url'])
+    return df_clean
+
 def load_dataset() -> pd.DataFrame:
     if os.path.exists(DATASET_PATH):
         print("Datset exists in folder. Skipping download.")
